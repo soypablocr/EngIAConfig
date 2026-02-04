769131498090d9344ef4f23d9b1e499f353ad5ce
soypablocr <soypabloarce@gmail.com>
2026-02-04 15:27:45 -0600

    chore: Update default application port from 5000 to 5001.

--- app.py
+++ app.py
@@ -100,4 +100,4 @@ def validate_params():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
